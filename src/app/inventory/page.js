e0833b710e1e28fe1f7edee2be1c9cca43012266
krisnaarianta <krisnaarianta@gmail.com>
2023-11-10 17:10:38 +0700

    feat: button add inventory

--- src/app/inventory/page.js
+++ src/app/inventory/page.js
@@ -1,3 +1,12 @@
-export default function Invetory(){
-    return <div>a</div>
+import React from 'react';
+
+export default function Inventory() {
+  return (
+    <div className="bg-white flex justify-between p-8">
+      <div></div>
+      <div>
+        <button className="bg-green-700 text-white px-4 py-2 rounded cursor-pointer font-poppins text-base sm:text-lg lg:text-xl">+Add Inventory</button>
+      </div>
+    </div>
+  );
 }
