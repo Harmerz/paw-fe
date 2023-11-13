d26958f78f830947df4dd64257574569a64943a5
nafisa <nafisaramadhania1122@gmail.com>
2023-11-13 08:23:11 +0700

    feat: fixing read recipe layout

--- src/app/recipe/more/page.js
+++ src/app/recipe/more/page.js
@@ -1,75 +1,78 @@
 'use client'
 
 import Image from 'next/image'
+import { useState } from 'react'
 import { IoColorWandSharp, IoTrashBinSharp } from 'react-icons/io5'
 
-export default function More() {
+export default function Pretty() {
+  const [data, setData] = useState([
+    {
+      img: 'https://th.bing.com/th/id/OIP.8kg0jQPbYYkcrYK9pJ1k8AAAAA?w=272&h=193&c=7&r=0&o=5&dpr=1.3&pid=1.7',
+      name: 'Telur Balado Pedas Cabe Hijau',
+      description:
+        'Telur Balado Pedas Cabe Hijau adalah hidangan lezat dan pedas yang terdiri dari telur rebus yang dimasak dengan bumbu balado khas Indonesia. Telur Balado Pedas Cabe Hijau sering dihidangkan sebagai lauk pendamping nasi putih hangat. Rasanya yang pedas dan sedikit manis membuat hidangan ini menjadi pilihan yang populer di berbagai acara makan, baik sehari-hari maupun pada kesempatan khusus.',
+      ingredients: [
+        '8 butir telur ayam yang direbus',
+        '1 lembar daun salam pilihan',
+        '1 batang serai yang telah dimemarkan',
+        '1 buah tomat hijau dengan ukuran besar atau sekitar 50 gr, lalu dipotong-potong',
+        '1 bungkus bumbu masak seperti Royco ayam',
+        '¼ sendok teh garam',
+        '1 sendok teh gula pasir',
+        '1 sendok makan air asam Jawa, dari larutan ½ sdt asam Jawa dan 2 sdm air',
+        '100 ml minyak goreng untuk menumis',
+        '5 butir bawang merah tumbuk kasar',
+        '10 buah cabai hijau besar atau berukuran kira-kira 150 gr tumbuk kasar',
+        '5 buah cabai hijau keriting segar tumbuk kasar',
+      ],
+      steps: [
+        'Tumis bawang merah, cabai hijau besar, cabai hijau keriting, serai, dan daun salam hingga tercium harum.',
+        'Selanjutnya masukkan tomat hijau, lalu tumis hingga layu.',
+        'Masukkan garam, telur, Royco ayam, dan gula pasir, lalu aduk hingga merata.',
+        'Tambahkan air asam Jawa dan masaklah sampai meresap.',
+        'Jika sudah meresap, Telur Balado Pedas Cabe Hijau siap dihidangkan.',
+      ],
+    },
+  ])
   return (
     <div className="relative flex bg-white pt-8">
       <div className="w-1/6 flex-none" />
-      <div>
-        <Image
-          src="https://th.bing.com/th/id/OIP.8kg0jQPbYYkcrYK9pJ1k8AAAAA?w=272&h=193&c=7&r=0&o=5&dpr=1.3&pid=1.7"
-          alt="Telur Balado Pedas Cabe Hijau"
-          width={200}
-          height={150}
-          className="mb-4"
-        />
-        <div>
-          <h1 className="mt-2 text-xl font-bold">Telur Balado Pedas Cabe Hijau</h1>
-          <p className="description">
-            Telur Balado Pedas Cabe Hijau adalah hidangan lezat dan pedas yang terdiri dari telur
-            rebus yang dimasak dengan bumbu balado khas Indonesia. Telur Balado Pedas Cabe Hijau
-            sering dihidangkan sebagai lauk pendamping nasi putih hangat. Rasanya yang pedas dan
-            sedikit manis membuat hidangan ini menjadi pilihan yang populer di berbagai acara makan,
-            baik sehari-hari maupun pada kesempatan khusus.
-          </p>
-        </div>
-        <div>
-          <br />
-          <h1 className="mt-2 text-xl font-bold">Bahan-Bahan</h1>
-          <ul className="ml-8 mr-24 list-disc text-left">
-            <li>8 butir telur ayam yang direbus</li>
-            <li>1 lembar daun salam pilihan</li>
-            <li>1 batang serai yang telah dimemarkan</li>
-            <li>1 buah tomat hijau dengan ukuran besar atau sekitar 50 gr, lalu dipotong-potong</li>
-            <li>1 bungkus bumbu masak seperti Royco ayam</li>
-            <li>¼ sendok teh garam</li>
-            <li>1 sendok teh gula pasir</li>
-            <li>1 sendok makan air asam Jawa, dari larutan ½ sdt asam Jawa dan 2 sdm air</li>
-            <li>100 ml minyak goreng untuk menumis</li>
-            <li>5 butir bawang merah tumbuk kasar</li>
-            <li>10 buah cabai hijau besar atau berukuran kira-kira 150 gr tumbuk kasar</li>
-            <li>5 buah cabai hijau keriting segar tumbuk kasar</li>
-          </ul>
-        </div>
-        <div>
-          <br />
-          <h1 className="mt-2 text-xl font-bold">Langkah-Langkah</h1>
-          <ul className="ml-8 mr-24 list-disc text-left">
-            <li>
-              Tumis bawang merah, cabai hijau besar, cabai hijau keriting, serai, dan daun salam
-              hingga tercium harum.
-            </li>
-            <li>Selanjutnya masukan tomat hijau, lalu tumis hingga layu.</li>
-            <li>Masukkan garam, telur, Royco ayam, dan gula pasir, lalu aduk hingga merata.</li>
-            <li>Tambahkan air asam Jawa dan masaklah sampai meresap.</li>
-            <li>Jika sudah meresap, Telur Balado Pedas Cabe Hijau siap dihidangkan.</li>
-          </ul>
-        </div>
-        <div className="bottom-0 right-0 mb-2 mr-2 flex items-center justify-end space-x-2">
-          <button
-            type="button"
-            className="flex cursor-pointer items-center rounded bg-ijo3 px-4 py-2 text-white"
-          >
-            <IoColorWandSharp className="mr-2" /> Edit
-          </button>
-          <button
-            type="button"
-            className="flex cursor-pointer items-center rounded bg-merah-tumbas px-4 py-2 text-white"
-          >
-            <IoTrashBinSharp className="mr-2" /> Delete
-          </button>
+      <div className="mt-4 flex">
+        <div className="mt-4 flex-col">
+          {data.map((item, index) => (
+            <div key={index} className="font-poppins my-2 p-4 text-base text-black">
+              <Image src={item.img} alt={item.name} width={300} height={225} className="mb-4" />
+              <h2 className="mb-2 text-2xl font-bold">{item.name}</h2>
+              <p>{item.description}</p>
+              <br />
+              <h3 className="mt-2 text-lg font-bold">Bahan-Bahan:</h3>
+              <ul className="ml-8 list-disc text-left">
+                {item.ingredients.map((ingredient, i) => (
+                  <li key={i}>{ingredient}</li>
+                ))}
+              </ul>
+              <h3 className="mt-2 text-lg font-bold">Langkah-Langkah:</h3>
+              <ol className="ml-8 list-decimal text-left">
+                {item.steps.map((step, i) => (
+                  <li key={i}>{step}</li>
+                ))}
+              </ol>
+            </div>
+          ))}
+          <div className="relative bottom-0 right-0 mb-2 mr-2 flex items-center justify-end space-x-2">
+            <button
+              type="button"
+              className="flex cursor-pointer items-center rounded bg-ijo3 px-4 py-2 text-white"
+            >
+              <IoColorWandSharp className="mr-2" /> Edit
+            </button>
+            <button
+              type="button"
+              className="flex cursor-pointer items-center rounded bg-merah-tumbas px-4 py-2 text-white"
+            >
+              <IoTrashBinSharp className="mr-2" /> Delete
+            </button>
+          </div>
         </div>
       </div>
       <div className="w-2/3 flex-grow bg-white" />
