ae192204e6c908f7c58bded1bf86d6638165b2f5
Haikal Hilmi <haikal6255hilmi@gmail.com>
2023-11-21 15:48:25 +0700

    feat : providers

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,26 @@
 import './globals.css'
 
+import PropTypes from 'prop-types'
+
+import { Providers } from '@/components'
+
 export const metadata = {
   title: 'Tumbas',
-  description: 'Tumbas by us',
+  description: 'Tumbas by Us',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Providers>
+          <main>{children}</main>
+        </Providers>
+      </body>
     </html>
   )
+}
+
+RootLayout.propTypes = {
+  children: PropTypes.isRequired,
 }
