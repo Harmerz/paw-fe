d0de62fdb1afd5c4cfbb4f390413c88ecc7afeff
Haikal Hilmi <haikal6255hilmi@gmail.com>
2023-12-07 17:33:37 +0700

    fix : middleware for dynamic routes

--- src/middlewares/adminMiddleware.js
+++ src/middlewares/adminMiddleware.js
@@ -1,20 +1,34 @@
 import { NextResponse } from 'next/server'
 import { getToken } from 'next-auth/jwt'
 
-export const adminPages = ['/inventory/create', '/recipe/create']
+export const adminPages = [
+  '/inventory/create',
+  '/recipe/create',
+  '/order/create',
+  '/delivery/create',
+  '/delivery/[id]',
+  '/order/[id]',
+  '/inventory/[id]',
+]
 
-const adminPathNameRegex = RegExp(`^/?(${adminPages.join('|')})?/?$`, 'i')
+const adminPathNameRegex = RegExp(
+  `^/?(${adminPages.map((page) => page.replace(/\[.*?\]/, '[^/]+')).join('|')})/?$`,
+  'i',
+)
 
 export async function adminMiddleware(req) {
   const isAdminPage = adminPathNameRegex.test(req.nextUrl.pathname)
   const token = await getToken({ req })
 
   if (isAdminPage) {
-    if (token.role === 'admin') {
+    if (token?.role === 'admin') {
       return NextResponse.next()
     }
-    // If user isn't an admin, redirect to homepage
-    return NextResponse.json("You don't have an permission", { status: 500 })
+    // If the user isn't an admin, return an error response
+    return NextResponse.json(
+      { error: "You don't have permission to access this resource" },
+      { status: 403 },
+    )
   }
 
   return undefined
