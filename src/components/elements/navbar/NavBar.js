41f090400f1d48569e39eecba5b00a57dc46429f
Haikal Hilmi <haikal6255hilmi@gmail.com>
2023-12-07 16:34:29 +0700

    feat : UX navbar with logout

--- src/components/elements/navbar/NavBar.js
+++ src/components/elements/navbar/NavBar.js
@@ -1,8 +1,11 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import { signOut, useSession } from 'next-auth/react'
 import { useEffect, useRef, useState } from 'react'
-import { IoPersonCircle } from 'react-icons/io5'
+import { IoLogOutOutline, IoPersonCircle } from 'react-icons/io5'
+
+import { axios } from '@/lib/axios'
 
 function Icon({ active, top }) {
   return (
@@ -21,12 +24,14 @@ function Icon({ active, top }) {
 }
 
 export function NavBar() {
+  const { data: session } = useSession()
   const pathname = usePathname()
   const [dropDown, setDropDown] = useState(false)
   const [yOffset, setYOffset] = useState(typeof window !== 'undefined' ? window?.scrollY : 0)
   const [top, setTop] = useState(pathname === '/')
   const ref = useRef(null)
   const [visible, setVisible] = useState(true)
+  const [logout, setLogout] = useState(false)
 
   useEffect(() => {
     window.addEventListener('click', (e) => {
@@ -66,7 +71,14 @@ export function NavBar() {
       path: '/recipe',
     },
   ]
-  console.log(dropDown)
+  async function HandleLogout(refreshToken) {
+    signOut()
+    await axios.delete('/auth/logout', {
+      headers: {
+        Authorization: `Bearer ${refreshToken}`,
+      },
+    })
+  }
   return (
     <nav className="relative flex h-full w-full flex-row justify-between border-b lg:px-10 lg:py-5">
       <Link href="/">
@@ -86,7 +98,26 @@ export function NavBar() {
           </Link>
         ))}
       </div>
-      <IoPersonCircle className="hidden h-10 w-10 text-xl text-black lg:flex" />
+      <button
+        type="button"
+        onClick={() => setLogout(!logout)}
+        className="relative flex flex-row items-center gap-3 text-black"
+      >
+        <IoPersonCircle className="hidden h-10 w-10 text-xl text-black lg:flex" />{' '}
+        {session.user.name}
+        {logout && (
+          <div className="absolute top-[100%] bg-white px-4 py-3 hover:bg-slate-200">
+            <button
+              key="Logout"
+              type="button"
+              onClick={() => HandleLogout(session?.user?.refreshToken)}
+              className=" flex w-[90px] flex-row items-center gap-2 text-red-600 hover:text-red-500"
+            >
+              <IoLogOutOutline className="h-5 w-5 text-xl" /> Log Out
+            </button>
+          </div>
+        )}
+      </button>
       <button
         ref={ref}
         type="button"
